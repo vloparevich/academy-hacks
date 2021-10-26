cc26bb6f9c41deffc2892e52cd8bb8d56c426aab
vloparevich <vadim.loparevich@gmail.com>
2021-10-26 19:47:04 -0400

    Init client and server

---
+++ prog-lang-tutor-server/routes/seeds/seed-route.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const Tutor = require('../../models/Tutor.model');
+const tutorsArray = require('../../bin/seeds');
+
+// ****************************************************************************************
+// GET route to seed the tutors
+// ****************************************************************************************
+router.get('/seed-my-db', async (req, res, next) => {
+  try {
+    await tutorsArray.forEach((tutor) => {
+      Tutor.create(tutor);
+    });
+    res.status(201).json({ success: true, message: 'DB has been seeded' });
+  } catch (err) {
+    res.json(
+      { success: false, message: 'Error while trying to seed the database' },
+      err
+    );
+  }
+});
+
+router.get('/seed-my-dbs', (req, res, next) => {
+  console.log('TEST');
+  res.status(201).json({ success: true, message: 'DB has been ruined' });
+});
+
+module.exports = router;
