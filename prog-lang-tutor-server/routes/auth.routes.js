82206527f3a5087a4696428bc2496ffc4951863f
Fernando Salcedo <fsalcedo55@hotmail.com>
2021-10-30 17:17:14 -0400

    Implement sign up route

---
+++ prog-lang-tutor-server/routes/auth.routes.js
@@ -0,0 +1,100 @@
+// routes/auth.routes.js
+
+const express = require("express");
+const router = express.Router();
+
+const passport = require("passport");
+const bcryptjs = require("bcryptjs");
+const mongoose = require("mongoose");
+
+const SALT_ROUNDS = 10;
+
+// require the user model !!!!
+const User = require("../models/User.model");
+
+router.post("/signup", (req, res, next) => {
+  const { email, password, firstName, lastName } = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({ message: "Provide email and password" });
+    return;
+  }
+
+  if (!firstName || !lastName) {
+    res.status(400).json({ message: "Provide first and last name" });
+    return;
+  }
+
+  // make sure passwords are strong:
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res.status(500).json({
+      errorMessage:
+        "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.",
+    });
+    return;
+  }
+
+  bcryptjs
+    .genSalt(SALT_ROUNDS)
+    .then((salt) => bcryptjs.hash(password, salt))
+    .then((hashedPassword) => {
+      return User.create({
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+        // password => this is the key from the User model
+        //     ^
+        //     |            |--> this is placeholder (how we named returning value from the previous method (.hash()))
+        password: hashedPassword,
+      });
+    })
+    .then((userFromDB) => {
+      console.log("Newly created user is: ", userFromDB);
+      // Send the user's information to the frontend
+      // We can use also: res.status(200).json(req.user);
+      res.status(200).json(userFromDB);
+    })
+    .catch((error) => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(500).json({ errorMessage: error.message });
+      } else if (error.code === 11000) {
+        res.status(500).json({
+          errorMessage: "Email need to be unique. Email is already used.",
+        });
+      } else {
+        next(error);
+      }
+    }); // close .catch()
+});
+
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, theUser, failureDetails) => {
+    if (err) {
+      res
+        .status(500)
+        .json({ message: "Something went wrong authenticating user" });
+      return;
+    }
+
+    if (!theUser) {
+      // "failureDetails" contains the error messages
+      // from our logic in "LocalStrategy" { message: '...' }.
+      res.status(401).json(failureDetails);
+      return;
+    }
+
+    // save user in session
+    req.login(theUser, (err) => {
+      if (err) {
+        res.status(500).json({ message: "Session save went bad." });
+        return;
+      }
+
+      // We are now logged in (that's why we can also send req.user)
+      res.status(200).json(theUser);
+    });
+  })(req, res, next);
+});
+
+module.exports = router;
