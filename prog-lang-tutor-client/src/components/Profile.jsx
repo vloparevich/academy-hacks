caba40410de04262066106bda3cdb5eef9520781
Dria Thomas <driathomas96@gmail.com>
2021-10-30 12:43:40 -0400

    "cloudinary  client update"

--- prog-lang-tutor-client/src/components/Profile.jsx
+++ prog-lang-tutor-client/src/components/Profile.jsx
@@ -20,12 +20,45 @@ class Profile extends Component {
                 this.setState({ ...user.data.tutor })
             })
     }
+    //profile pic 
+    handleFileUpload = e => {
+        const uploadData = new FormData();
+        uploadData.append('profilePic', e.target.files[0]);
+
+        service
+            .handleUpload(uploadData)
+            .then(response => {
+
+                this.setState({ profilePic: response.secure_url });
+            })
+            .catch(err => console.log('Error while uploading the file: ', err));
+    };
+
+
+    // submits the profile pic form
+    handleSubmit = e => {
+        e.preventDefault();
+
+        service
+            .saveNewMovie(this.state)
+            .then(res => {
+                console.log(res);
+
+                redirect('')
+            })
+            .catch(err => console.log('Error while adding the new movie: ', err));
+    };
 
     render() {
         console.log(this.state.firstName)
         return (
             <div className="container">
-                <div className="userImage"><p>user image to go here</p></div>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="userImage"><p>user image to go here</p></div>
+                    <input type="file" onChange={this.handleFileUpload} />
+
+                    <button type="submit">Save Profile Pic</button>
+                </form>
                 <div>
                     <div className="userDetails">
                         <li>Name:</li>
