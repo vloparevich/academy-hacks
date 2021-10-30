3b29ca578870c5cdf7fb64beadbfc060b346337a
jlp21 <portelli21@gmail.com>
2021-10-30 12:08:00 -0400

    Added home and Profile pages

---
+++ prog-lang-tutor-client/src/components/Profile.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+
+class Profile extends Component {
+
+    state = {};
+
+    componentDidMount() {
+        this.getUser()
+    }
+
+    getUser = () => {
+        axios.get(`http://localhost:5000/api/user/tutor/617b378c30aceadaa78445fb`)
+
+        .then(user => {
+            console.log(user.data.tutor)
+            this.setState({...user.data.tutor})
+        })
+    }
+
+    render() {
+        console.log(this.state.firstName)
+        return (
+            <div className="container">
+            <div className="userImage"><p>user image to go here</p></div>
+            <div>
+            <div className="userDetails">
+                <li>Name:</li>
+                <li>Looking To Learn:</li>
+                <li>Days/Time Available</li>
+            </div>
+            <div className="bookedSessions"><p>sessions calendar to go here</p></div>
+            <div>HELLO {this.state.firstName}</div>
+            </div>
+
+
+                
+            </div>
+        );
+    }
+}
+
+export default Profile;
