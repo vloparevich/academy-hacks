25b1b7fa6bb8a288875652a7058f3aa66308b9ea
Fernando Salcedo <fsalcedo55@hotmail.com>
2021-11-02 19:31:14 -0400

    Implement sign up -  WIP

---
+++ prog-lang-tutor-client/src/components/Auth/auth-service.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "http://localhost:5000/auth",
+  withCredentials: true,
+});
+
+const authService = {
+  signup: (body) =>
+    instance.post("/signup", body).then((response) => response.data),
+  getSession: () => instance.get("/session").then((response) => response.data),
+};
+
+export default authService;
