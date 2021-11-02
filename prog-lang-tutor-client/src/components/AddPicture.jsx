a9a4ffb716e24acdee0d2bdb3e0a38c0f24475d8
Dria Thomas <driathomas96@gmail.com>
2021-11-02 14:03:43 -0400

    update style

--- prog-lang-tutor-client/src/components/AddPicture.jsx
+++ prog-lang-tutor-client/src/components/AddPicture.jsx
@@ -49,7 +49,6 @@ function AddPicture() {
 
     return (
         <div>
-
             <form onSubmit={handleSubmit}>
 
 
--- prog-lang-tutor-client/src/components/Profile.jsx
+++ prog-lang-tutor-client/src/components/Profile.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Timeslot from './Timeslot/Timeslot';
+import AddPicture from './AddPicture/AddPicture';
 
 class Profile extends Component {
     state = {
@@ -23,64 +24,72 @@ class Profile extends Component {
             })
     }
     //profile pic 
-    handleFileUpload = e => {
-        const uploadData = new FormData();
-        uploadData.append('profilePic', e.target.files[0]);
+    // handleFileUpload = e => {
+    //     const uploadData = new FormData();
+    //     uploadData.append('profilePic', e.target.files[0]);
 
-        service
-            .handleUpload(uploadData)
-            .then(response => {
+    //     service
+    //         .handleUpload(uploadData)
+    //         .then(response => {
 
-                this.setState({ profilePic: response.secure_url });
-            })
-            .catch(err => console.log('Error while uploading the file: ', err));
-    };
+    //             this.setState({ profilePic: response.secure_url });
+    //         })
+    //         .catch(err => console.log('Error while uploading the file: ', err));
+    // };
 
 
-    // submits the profile pic form
-    handleSubmit = e => {
-        e.preventDefault();
+    // // submits the profile pic form
+    // handleSubmit = e => {
+    //     e.preventDefault();
 
-        service
-            .saveNewMovie(this.state)
-            .then(res => {
-                console.log(res);
+    //     service
+    //         .saveNewMovie(this.state)
+    //         .then(res => {
+    //             console.log(res);
 
-                redirect('')
-            })
-            .catch(err => console.log('Error while adding the new movie: ', err));
-    };
+    //             redirect('')
+    //         })
+    //         .catch(err => console.log('Error while adding the new movie: ', err));
+    // };
 
     render() {
         console.log(this.state.firstName)
         return (
+
             <div className="container">
+
+
+                <AddPicture />
+
+
                 <form onSubmit={this.handleSubmit}>
                     <div className="userImage"><p>user image to go here</p></div>
                     <input type="file" onChange={this.handleFileUpload} />
 
                     <button type="submit">Save Profile Pic</button>
                 </form>
-                <div>
-                    <div className="userDetails">
-                        <li>Name:</li>
-                        <li>Looking To Learn:</li>
-                        <li>Days/Time Available</li>
-                    </div>
-                    <div className="bookedSessions"><p>sessions calendar to go here</p></div>
-                    <div>HELLO {this.state.firstName}</div>
-                </div>
 
-      .then((user) => {
+                <div className="userDetails">
+
+                    <li>Name:</li>
+                    <li>Looking To Learn:</li>
+                    <li>Days/Time Available</li>
+                </div>
+                <div className="bookedSessions"><p>sessions calendar to go here</p></div>
+                <div>HELLO {this.state.firstName}</div>
+
+
+
+                {/* .then((user) => {
                     console.log(user.data.tutor);
                 this.setState({...user.data.tutor});
       });
-  };
+  }; */}
 
 
-                </div>
-                );
-  }
+            </div>
+        );
+    }
 }
 
 export default Profile;
