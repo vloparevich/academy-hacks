0f4db1d37f46bb02a02524a8007d891f5a47ba4c
Dria Thomas <driathomas96@gmail.com>
2021-11-02 14:03:02 -0400

    update

--- prog-lang-tutor-client/src/App.js
+++ prog-lang-tutor-client/src/App.js
@@ -1,15 +1,8 @@
 import './App.css';
-<<<<<<< HEAD
-// import Timeslot from './components/Timeslot/Timeslot';
-import React from 'react';
-import Profile from './components/Profile';
-import Home from './components/Home';
-=======
 import React from 'react';
 import Profile from './components/Profile/Profile';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
->>>>>>> 44b296c2a0deca551d2fcd5fd5324205b255da23
 import { Switch, Route } from 'react-router-dom'
 
 class App extends React.Component {
--- prog-lang-tutor-client/src/components/Profile.jsx
+++ prog-lang-tutor-client/src/components/Profile.jsx
@@ -77,45 +77,10 @@ class Profile extends Component {
       });
   };
 
-  handleRange = (event) => {
-    const {name, value} = event.target;
-                this.setState({[name]: value }, () => console.log(this.state));
-  };
 
-                render() {
-                    console.log(this.state.firstName);
-                return (
-                <div className='container'>
-                    <div className='userImage'>
-                        <p>user image to go here</p>
-                    </div>
-                    <div>
-                        <div className='userDetails'>
-                            <li>Name:</li>
-                            <li>Looking To Learn:</li>
-                            <li>Days/Time Available</li>
-                            <label>FROM:</label>
-                            <input
-                                type='number'
-                                name='from'
-                                onChange={(event) => this.handleRange(event)}
-                            />
-                            <label>TO:</label>
-                            <input
-                                type='number'
-                                name='to'
-                                onChange={(event) => this.handleRange(event)}
-                            />
-                            <button onClick={this.handleTmieRange}>Continue</button>
-                        </div>
-                        <div className='bookedSessions'>
-                            <p>sessions calendar to go here</p>
-                        </div>
-                        <div>HELLO {this.state.firstName}</div>
-                    </div>
                 </div>
                 );
   }
 }
 
-                export default Profile;
+export default Profile;
