3fe25d163c4e687a2da32fdf494a467011c50598
Dria Thomas <driathomas96@gmail.com>
2021-11-02 20:57:41 -0400

    updated css

--- prog-lang-tutor-client/src/components/Profile.jsx
+++ prog-lang-tutor-client/src/components/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import Timeslot from './Timeslot/Timeslot';
+// import Timeslot from './Timeslot/Timeslot';
 import AddPicture from './AddPicture/AddPicture';
 
 class Profile extends Component {
