574e431d3ef9d1926476140ad264014d0077251a
vloparevich <vadim.loparevich@gmail.com>
2021-11-08 07:28:01 -0500

    Add two more fields to be saved upon signup

--- prog-lang-tutor-client/src/components/Auth/Signup.jsx
+++ prog-lang-tutor-client/src/components/Auth/Signup.jsx
@@ -1,17 +1,17 @@
-import React, { Component } from "react";
-import { signup } from "../../services/auth-service";
-import "./Auth.css";
-import * as USER_HELPERS from "../../utils/userToken";
-import * as PATHS from "../../utils/paths";
+import React, { Component } from 'react';
+import { signup } from '../../services/auth-service';
+import './Auth.css';
+import * as USER_HELPERS from '../../utils/userToken';
+import * as PATHS from '../../utils/paths';
 
 export default class Signup extends Component {
   state = {
-    email: "",
-    password: "",
-    firstName: "",
-    lastName: "",
-    countryOfOrigin: "",
-    teachingExperience: "",
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    countryOfOrigin: '',
+    teachingExperience: '',
     isTutor: false,
   };
 
@@ -29,6 +29,8 @@ export default class Signup extends Component {
       firstName: this.state.firstName,
       lastName: this.state.lastName,
       isTutor: this.state.isTutor,
+      countryOfOrigin: this.state.countryOfOrigin,
+      teachingExperience: this.state.teachingExperience,
     };
 
     signup(signingDetails).then((res) => {
@@ -37,7 +39,7 @@ export default class Signup extends Component {
       if (!res.status) {
         // unsuccessful signup
       }
-      console.log("the token", { token: res.data });
+      console.log('the token', { token: res.data });
 
       USER_HELPERS.setUserToken(res.data.accessToken);
 
@@ -62,19 +64,19 @@ export default class Signup extends Component {
 
   render() {
     return (
-      <div className="auth-container">
-        <ul className="auth-top-bar">
+      <div className='auth-container'>
+        <ul className='auth-top-bar'>
           {!this.state.isTutor ? (
             <>
               <button
                 onClick={this.handleStudentSelection}
-                id="active-student-signup-toggle"
+                id='active-student-signup-toggle'
               >
                 Student
               </button>
               <button
                 onClick={this.handleTutorSelection}
-                id="inactive-student-signup-toggle"
+                id='inactive-student-signup-toggle'
               >
                 Tutor
               </button>
@@ -83,13 +85,13 @@ export default class Signup extends Component {
             <>
               <button
                 onClick={this.handleStudentSelection}
-                id="inactive-student-signup-toggle"
+                id='inactive-student-signup-toggle'
               >
                 Student
               </button>
               <button
                 onClick={this.handleTutorSelection}
-                id="active-tutor-signup-toggle"
+                id='active-tutor-signup-toggle'
               >
                 Tutor
               </button>
@@ -98,7 +100,7 @@ export default class Signup extends Component {
         </ul>
         <form
           onSubmit={this.handleFormSubmission}
-          className="auth-form-container"
+          className='auth-form-container'
         >
           {!this.state.isTutor ? (
             <>
@@ -106,44 +108,44 @@ export default class Signup extends Component {
               <h2>Let's begin your next adventure</h2>
               <label>First Name</label>
               <input
-                type="text"
-                name="firstName"
-                placeholder="First Name"
+                type='text'
+                name='firstName'
+                placeholder='First Name'
                 value={this.state.firstName}
                 onChange={this.changeHandler}
               />
 
               <label>Last Name</label>
               <input
-                type="text"
-                name="lastName"
-                placeholder="Last Name"
+                type='text'
+                name='lastName'
+                placeholder='Last Name'
                 value={this.state.lastName}
                 onChange={this.changeHandler}
               />
 
               <label>Enter your email</label>
               <input
-                type="email"
-                name="email"
-                placeholder="Enter your email"
+                type='email'
+                name='email'
+                placeholder='Enter your email'
                 value={this.state.email}
                 onChange={this.changeHandler}
               />
 
               <label>Create a password</label>
               <input
-                type="password"
-                name="password"
-                placeholder="Create a password"
-                autoComplete="current-password"
+                type='password'
+                name='password'
+                placeholder='Create a password'
+                autoComplete='current-password'
                 value={this.state.password}
                 onChange={this.changeHandler}
               />
               <button
-                type="submit"
-                value="Create account"
-                className="auth-button"
+                type='submit'
+                value='Create account'
+                className='auth-button'
               >
                 Create a student account
               </button>
@@ -154,27 +156,27 @@ export default class Signup extends Component {
               <h2>Start a rewarding career!</h2>
               <label>First Name</label>
               <input
-                type="text"
-                name="firstName"
-                placeholder="First Name"
+                type='text'
+                name='firstName'
+                placeholder='First Name'
                 value={this.state.firstName}
                 onChange={this.changeHandler}
               />
 
               <label>Last Name</label>
               <input
-                type="text"
-                name="lastName"
-                placeholder="Last Name"
+                type='text'
+                name='lastName'
+                placeholder='Last Name'
                 value={this.state.lastName}
                 onChange={this.changeHandler}
               />
 
               <label>How many years have you been teaching?</label>
               <input
-                type="number"
-                name="teachingExperience"
-                min="1"
+                type='number'
+                name='teachingExperience'
+                min='1'
                 // placeholder="How many years have you been teaching?"
                 value={this.state.teachingExperience}
                 onChange={this.changeHandler}
@@ -182,35 +184,35 @@ export default class Signup extends Component {
 
               <label>What country are you from?</label>
               <input
-                type="text"
-                name="countryOfOrigin"
-                placeholder="What country are you from?"
+                type='text'
+                name='countryOfOrigin'
+                placeholder='What country are you from?'
                 value={this.state.countryOfOrigin}
                 onChange={this.changeHandler}
               />
 
               <label>Enter your email</label>
               <input
-                type="email"
-                name="email"
-                placeholder="Enter your email"
+                type='email'
+                name='email'
+                placeholder='Enter your email'
                 value={this.state.email}
                 onChange={this.changeHandler}
               />
 
               <label>Create a password</label>
               <input
-                type="password"
-                name="password"
-                placeholder="Create a password"
-                autoComplete="current-password"
+                type='password'
+                name='password'
+                placeholder='Create a password'
+                autoComplete='current-password'
                 value={this.state.password}
                 onChange={this.changeHandler}
               />
               <button
-                type="submit"
-                value="Create account"
-                className="auth-button"
+                type='submit'
+                value='Create account'
+                className='auth-button'
               >
                 Create a tutor account
               </button>
