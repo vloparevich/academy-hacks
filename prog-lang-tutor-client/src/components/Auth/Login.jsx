42b112a758be3501b075a34d937eb74878e13602
vloparevich <vadim.loparevich@gmail.com>
2021-11-08 16:26:07 -0500

    Fix error upon incorrect password

--- prog-lang-tutor-client/src/components/Auth/Login.jsx
+++ prog-lang-tutor-client/src/components/Auth/Login.jsx
@@ -1,13 +1,14 @@
-import React, { Component } from "react";
-import { login } from "../../services/auth-service";
-import * as PATHS from "../../utils/paths";
-import * as USER_HELPERS from "../../utils/userToken";
-import "./Auth.css";
+import React, { Component } from 'react';
+import { login } from '../../services/auth-service';
+import * as PATHS from '../../utils/paths';
+import * as USER_HELPERS from '../../utils/userToken';
+import './Auth.css';
 
 export default class Login extends Component {
   state = {
-    email: "",
-    password: "",
+    email: '',
+    password: '',
+    attempts: 3,
   };
 
   changeHandler = (e) => {
@@ -27,42 +28,70 @@ export default class Login extends Component {
       if (!res.status) {
         // handle not great request
       }
-      USER_HELPERS.setUserToken(res.data.accessToken);
-      this.props.authenticate(res.data.user);
-      this.props.history.push(PATHS.HOMEPAGE);
+      if (res.data?.accessToken) {
+        USER_HELPERS.setUserToken(res.data.accessToken);
+        this.props.authenticate(res.data.user);
+        this.props.history.push(PATHS.HOMEPAGE);
+      } else {
+        this.setState({ invalidCredentials: true });
+      }
     });
+  };
+
+  handleAttempts = () => {
+    this.setState({ attempts: this.state.attempts - 1 });
   };
 
   render() {
     return (
       <div>
-        <div className="auth-container">
+        <div className='auth-container'>
           <h2>Sign in to Academy Hacks</h2>
           <form
             onSubmit={this.handleFormSubmission}
-            className="auth-form-container"
+            className='auth-form-container'
           >
             <label>Enter your email</label>
             <input
-              type="email"
-              name="email"
-              placeholder="Enter your email"
+              type='email'
+              name='email'
+              placeholder='Enter your email'
               value={this.state.email}
               onChange={this.changeHandler}
             />
 
             <label>Enter your password</label>
             <input
-              type="password"
-              name="password"
-              placeholder="Enter your password"
-              autoComplete="current-password"
+              type='password'
+              name='password'
+              placeholder='Enter your password'
+              autoComplete='current-password'
               value={this.state.password}
               onChange={this.changeHandler}
             />
-            <button type="submit" value="Log In" className="auth-button">
-              Log In
-            </button>
+            {this.state.invalidCredentials && this.state.attempts ? (
+              <div className='invalidCredentials'>
+                <p>You have entered an invalid username or password</p>
+                <p>Attempts left: {this.state.attempts}</p>
+              </div>
+            ) : (
+              <></>
+            )}
+            {this.state.attempts ? (
+              <button
+                onClick={this.handleAttempts}
+                type='submit'
+                value='Log In'
+                className='auth-button'
+              >
+                Log In
+              </button>
+            ) : (
+              <div className='invalidCredentials'>
+                <p>You’ve reached the maximum logon attempts.</p>
+                <p>Exit your browser and try again.</p>
+              </div>
+            )}
           </form>
         </div>
       </div>
