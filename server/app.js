3e62d2a36677454005243d6f78b4242149c26766
vloparevich <vadim.loparevich@gmail.com>
2021-11-19 13:54:47 -0500

    Add headers to prevent CORS issue

--- server/app.js
+++ server/app.js
@@ -2,14 +2,14 @@
 
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
-require("dotenv/config");
+require('dotenv/config');
 
 // ℹ️ Connects to the database
-require("./db");
+require('./db');
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
@@ -23,41 +23,51 @@ const app = express();
 // );
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
-require("./config")(app);
+require('./config')(app);
 
-const projectName = "prog-lang-tutor-server";
+const projectName = 'prog-lang-tutor-server';
 const capitalized = (string) =>
   string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
 
-// 👇 Start handling routes here
-const index = require("./routes/index");
-app.use("/", index);
+// NEW - Add CORS headers - see https://enable-cors.org/server_expressjs.html
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
-const authRoutes = require("./routes/auth.routes");
-app.use("/api/auth", authRoutes);
+// 👇 Start handling routes here
+const index = require('./routes/index');
+app.use('/', index);
+
+const authRoutes = require('./routes/auth.routes');
+app.use('/api/auth', authRoutes);
 
 // const studentRoutes = require('./routes/student.routes');
 // app.use('/api/student', studentRoutes);
 
-const userRoutes = require("./routes/user.routes");
-app.use("/api/user", userRoutes);
+const userRoutes = require('./routes/user.routes');
+app.use('/api/user', userRoutes);
 
 // To be used for reinitializing the DB after it is dropped
-app.use("/api/seed", require("./routes/seed.routes"));
+app.use('/api/seed', require('./routes/seed.routes'));
 
-app.use("/api/booking", require("./routes/booking.routes"));
+app.use('/api/booking', require('./routes/booking.routes'));
 
-app.use("/api/profile", require("./routes/profile.routes"));
+app.use('/api/profile', require('./routes/profile.routes'));
 
-app.use("/api/lesson", require("./routes/lesson.routes"));
+app.use('/api/lesson', require('./routes/lesson.routes'));
 
 app.use('/api/review', require('./routes/tutorReview.routes'));
 
 // app.use('/student', require('./routes/student.routes'));
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
+require('./error-handling')(app);
 
 module.exports = app;
