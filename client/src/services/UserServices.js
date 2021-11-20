d840a4d3f595503824fc5158ded473a5bbb22ed7
Dria Thomas <driathomas96@gmail.com>
2021-11-19 22:44:37 -0500

    hompage responsive

--- client/src/services/UserServices.js
+++ client/src/services/UserServices.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const service = axios.create({
-  baseURL: 'http://localhost:5000/api/user',
+  baseURL: `${process.env.REACT_APP_SERVER_URL}/user`,
 });
 
 const USER_SERVICE = {
