9a1531fb8cdbb23d3b748b99915dd584a8e2ef88
vloparevich <vadim.loparevich@gmail.com>
2021-11-19 22:36:34 -0500

    Add 3rd headers

--- server/app.js
+++ server/app.js
@@ -37,6 +37,7 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Origin',
     'https://the-academy-hacks.netlify.app'
   );
+  res.header('Access-Control-Allow-Credentials', 'true');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
