6e56bd6828330ad4429aa9509ec4f0bbdb32620c
vloparevich <vadim.loparevich@gmail.com>
2021-11-19 22:15:15 -0500

    Add fixed  origin

--- server/app.js
+++ server/app.js
@@ -35,7 +35,8 @@ app.use((req, res, next) => {
   console.log('HITTING THE ACCES-CONTROL...');
   res.header(
     'Access-Control-Allow-Origin',
-    'https://the-academy-hacks.netlify.app'
+    // 'https://the-academy-hacks.netlify.app'
+    '*'
   );
   next();
 });
