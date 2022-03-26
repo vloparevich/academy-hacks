7a84835b0c6cb6d95a229f235a91f11f7d7dc111
vloparevich <vadim.loparevich@gmail.com>
2022-03-25 21:19:37 -0400

    Refactor class cmp into func cmp

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,45 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import Time from '../Time/Time';
 import NavBarButtonsUserNotAuth from './NavBarButtonsUserNotAuth';
 import NavBarButtonsUserAuthorized from './NavBarButtonsUserAuthorized';
 
-class Navbar extends Component {
-  state = {
-    navBarClicked: false,
-  };
-
-  applyStyle = () => {
-    this.setState({ navBarClicked: !this.state.navBarClicked });
-  };
-
-  render() {
-    if (this.props.loading) {
-      return <div>Loading...</div>;
-    }
-    return (
-      <navbar id='mainNavComponent'>
-        <Link to='/'>
-          <img id='logo' src='../logo.png' alt='logo' />
-        </Link>
-        <Time />
-
-        {this.props.user ? (
-          <NavBarButtonsUserAuthorized
-            path='/user'
-            firstName={this.props.user.firstName}
-            handleLogout={this.props.handleLogout}
-            greeting={
-              this.props.user.isTutor ? 'Tutor Dashboard' : 'Student Dashboard'
-            }
-          />
-        ) : (
-          <NavBarButtonsUserNotAuth />
-        )}
-      </navbar>
-    );
+export default function Navbar(props) {
+  if (props.loading) {
+    return <div>Loading...</div>;
   }
-}
+  return (
+    <navbar id='mainNavComponent'>
+      <Link to='/'>
+        <img id='logo' src='../logo.png' alt='logo' />
+      </Link>
+      <Time />
 
-export default Navbar;
+      {props.user ? (
+        <NavBarButtonsUserAuthorized
+          path='/user'
+          firstName={props.user.firstName}
+          handleLogout={props.handleLogout}
+          greeting={
+            props.user.isTutor ? 'Tutor Dashboard' : 'Student Dashboard'
+          }
+        />
+      ) : (
+        <NavBarButtonsUserNotAuth />
+      )}
+    </navbar>
+  );
+}
