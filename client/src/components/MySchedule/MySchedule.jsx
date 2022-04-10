c9e81f49893c2709a85c3d256d03fcf59f959806
vloparevich <vadim.loparevich@gmail.com>
2022-04-10 18:52:15 -0400

    Refactor Signup by conv into func component

--- client/src/components/MySchedule/MySchedule.jsx
+++ client/src/components/MySchedule/MySchedule.jsx
@@ -1,33 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './MySchedule.css';
 
-export default class MyScedule extends Component {
-  render() {
-    return (
-      <>
-        {this.props.mySchedule?.map((el, i) => (
-          <>
-            {el.bookedTime.length > 0 && (
-              <div>
-                <div className='myLessonCard' key={i}>
-                  {el.bookedTime.length > 0 && (
-                    <>
-                      <p>{el.date}</p>
-                      <ul>
-                        {el.bookedTime.map((time, i) => (
-                          <li key={i}>
-                            {time}:00 - {time + 1}:00
-                          </li>
-                        ))}
-                      </ul>
-                    </>
-                  )}
-                </div>
+export default function MyScedule(props) {
+  return (
+    <React.Fragment>
+      {props.mySchedule?.map((el, i) => (
+        <>
+          {el.bookedTime.length > 0 && (
+            <div>
+              <div className='myLessonCard' key={i}>
+                {el.bookedTime.length > 0 && (
+                  <>
+                    <p>{el.date}</p>
+                    <ul>
+                      {el.bookedTime.map((time, i) => (
+                        <li key={i}>
+                          {time}:00 - {time + 1}:00
+                        </li>
+                      ))}
+                    </ul>
+                  </>
+                )}
               </div>
-            )}
-          </>
-        ))}
-      </>
-    );
-  }
+            </div>
+          )}
+        </>
+      ))}
+    </React.Fragment>
+  );
 }
