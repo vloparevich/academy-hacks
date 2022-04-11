1d3ed21086522482b4bd0bc77f3f9cb4e7d6b0b0
vloparevich <vadim.loparevich@gmail.com>
2022-04-11 17:21:24 -0400

    Optimize component structure

--- client/src/components/Auth/StudentSignUpForm.jsx
+++ client/src/components/Auth/StudentSignUpForm.jsx
@@ -2,45 +2,36 @@ import React from 'react';
 import InputSignup from './InputSignup';
 
 export default function StudentSignUpForm(props) {
-  const {
-    firstName,
-    lastName,
-    email,
-    password,
-    handleFormSubmission,
-    onChange,
-  } = props;
-
   const inputDetails = [
     {
       type: 'text',
       name: 'firstName',
       placeholder: 'First Name',
-      value: firstName,
+      value: props.firstName,
     },
     {
       type: 'text',
       name: 'lastName',
       placeholder: 'Last Name',
-      value: lastName,
+      value: props.lastName,
     },
     {
       type: 'email',
       name: 'email',
       placeholder: 'Enter your email',
-      value: email,
+      value: props.email,
     },
     {
       type: 'password',
       name: 'password',
       placeholder: 'Create a password',
       autoComplete: 'current-password',
-      value: password,
+      value: props.password,
     },
   ];
 
   return (
-    <form onSubmit={handleFormSubmission} className='auth-form-container'>
+    <form onSubmit={props.handleFormSubmission} className='auth-form-container'>
       <h3>Sign up as a student</h3>
       <h2>Let's begin your next adventure</h2>
       {inputDetails.map((thisInput) => (
@@ -50,7 +41,7 @@ export default function StudentSignUpForm(props) {
           placeholder={thisInput.placeholder}
           value={thisInput.value}
           autoComplete={thisInput.autoComplete}
-          onChange={onChange}
+          onChange={props.onChange}
         />
       ))}
       <button type='submit' value='Create account' className='auth-button'>
